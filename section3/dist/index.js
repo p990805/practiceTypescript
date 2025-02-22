f3fab01f65a4f3fffad905a4a9c1e485cbbed7eb
박주찬 <p990805@naver.com>
2025-02-23 02:14:45 +0900

    [주찬] Feat : 타입 추론, 타입 단언, 타입 좁히기, 서로소 유니온 타입 공부 02.23

---
+++ section3/dist/index.js
@@ -0,0 +1,2 @@
+console.log("hellow section3");
+export {};
---
+++ section3/src/chapter2.ts
@@ -0,0 +1,17 @@
+
+function unknownExam(){
+    let a:unknown =1;
+    let b:unknown= "hello";
+    let c:unknown = true;
+}
+
+
+// function neverExam=(){
+//     function neverFunc(): never{
+//         while(true){}
+//     }
+
+//     let num: string = neverFunc();
+// }
+
+  
